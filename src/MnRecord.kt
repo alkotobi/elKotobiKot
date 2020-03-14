d2b2f85fce346328d52ac0ffc9e1801af90a256a
merhab <merhab@gmail.com>
2020-03-14 21:29:07 +0800

    start the Project created a get fields fun

---
+++ src/MnRecord.kt
@@ -0,0 +1,39 @@
+package com.merhab
+
+import java.lang.reflect.Field
+
+open class MnRecord(var id: Int =-1) {
+    private var n =-1
+    protected var m =-1
+    open var t =-1
+
+
+
+    fun getClassName(): String {
+        return this.javaClass.name
+    }
+
+    protected fun getSupClassesArray():Array<Class<*>>{
+        var ar :MutableList<Class<*>> = mutableListOf(this.javaClass)
+        var cls:Class<*> = this.javaClass
+        while (cls.name!="com.merhab.MnRecord"){
+            cls=cls.superclass
+            ar.add(cls)
+        };
+        return ar.toTypedArray();
+
+    }
+
+    fun getFieldsArray():Array<Field>{
+        var ar :MutableList<Field> = mutableListOf()
+       for (cls in getSupClassesArray()){
+           ar.addAll(cls.declaredFields)
+       }
+        return ar.toTypedArray()
+    }
+
+}
+
+class MnRd(id: Int =-1) : MnRecord(id) {
+    var nono =-1
+}
